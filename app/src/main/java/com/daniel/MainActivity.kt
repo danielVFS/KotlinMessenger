b5dc19ace3da9deacb7a7fb20d7dc8c247ddf2b5
danielVFS <danielv759153@gmail.com>
2021-07-17 09:08:39 -0300

    UI: login and register

--- app/src/main/java/com/daniel/MainActivity.kt
+++ app/src/main/java/com/daniel/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.daniel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button_login.setOnClickListener {
+            val email = email_edit_text_login.text.toString()
+            val password = password_edit_text_login.text.toString()
+        }
+
+        already_have_and_account_text_view.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
