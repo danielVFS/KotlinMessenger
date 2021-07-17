c9e50b15982f910f9f398a40ca1e801f9f9c522b
danielVFS <danielv759153@gmail.com>
2021-07-17 10:36:01 -0300

    signUp User

--- app/src/main/java/com/daniel/MainActivity.kt
+++ app/src/main/java/com/daniel/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +17,16 @@ class MainActivity : AppCompatActivity() {
         register_button_register.setOnClickListener {
             val email = email_edit_text_register.text.toString()
             val password = password_edit_text_register.text.toString()
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d("MainActivity", "signInWithEmail:success")
+                    } else {
+                        Log.d("MainActivity", "signInWithEmail:failure", it.exception)
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
         already_have_and_account_text_view.setOnClickListener{
