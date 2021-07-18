cd6ec611592845f334f53aadd017a745c21bd01f
danielVFS <danielv759153@gmail.com>
2021-07-18 14:03:05 -0300

    add groupie and picasso dependecies

--- app/src/main/java/com/daniel/NewMessageActivity.kt
+++ app/src/main/java/com/daniel/NewMessageActivity.kt
@@ -3,12 +3,22 @@ package com.daniel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
 
-        supportActionBar?.title = Html.fromHtml("<font color=\"#ffffff\">" + getString(R.string.app_name) + "</font>")
+        supportActionBar?.title = Html.fromHtml("<font color=\"#ffffff\">" + "Select User" + "</font>")
+
+        val adapter = GroupAdapter<ViewHolder>()
+
+        recyclerview_new_message.adapter = adapter
     }
 }
