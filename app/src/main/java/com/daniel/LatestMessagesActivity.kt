85d13da88726d198c8b286a0083c737e92ace3d9
danielVFS <danielv759153@gmail.com>
2021-07-18 09:57:15 -0300

    changing on TabBar

--- app/src/main/java/com/daniel/LatestMessagesActivity.kt
+++ app/src/main/java/com/daniel/LatestMessagesActivity.kt
@@ -1,11 +1,26 @@
 package com.daniel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
+
+        verifyIfUserIsLoggedIn()
+    }
+
+    private fun verifyIfUserIsLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+
+        if(uid == null) {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
     }
 }
